b5ba993c601eedf513d81385255b33b078522934
rrrix1 <rrrix@bogusthunder.net>
2014-09-05 09:39:46 -0700

    Makin this shit 2.1!

--- QuestTools.cs
+++ QuestTools.cs
@@ -10,7 +10,7 @@ namespace QuestTools
 {
     public class QuestTools
     {
-        public static Version PluginVersion = new Version(2, 0, 100);
+        public static Version PluginVersion = new Version(2, 1, 1);
 
         private static int _skipEventDuration = -1;
         private static readonly Stopwatch SkipEventTimer = new Stopwatch();
